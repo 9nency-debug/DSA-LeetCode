90543ca8ba2cf2cb02777181d04ef27bd883f01a
Nency <9.nency@gmail.com>
2026-04-20 22:24:33 +0530

    Time: 0 ms (100%), Space: 12 MB (72.82%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -3,6 +3,7 @@ public:
     int maxDistance(vector<int>& colors) {
         int n = colors.size();
         int ans = 0;
+        
         for (int j = n - 1; j >= 0; j--) {
             if (colors[j] != colors[0]) {
                 ans = max(ans, j);
