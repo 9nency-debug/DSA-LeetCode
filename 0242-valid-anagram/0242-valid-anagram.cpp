ff383c100b335607d7f102a4bd635ff2a6b935c8
Nency <9.nency@gmail.com>
2026-04-23 22:59:16 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.95%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        unordered_map<char, int> freq;
+        for (char ch : s) {
+            freq[ch]++;
+        }
+        for (char ch : t) {
+            if (freq[ch] == 0) {
+                return false;
+            }
+            freq[ch]--;
+        }
+
+        return true;
+    }
+};
