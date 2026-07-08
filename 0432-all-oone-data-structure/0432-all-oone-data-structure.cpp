7e48c63217d70d0d02bd25c313f50fc95a8ab949
Nency <9.nency@gmail.com>
2026-07-08 18:14:36 +0530

    Time: 258 ms (7.23%), Space: 77.6 MB (99.54%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -1,30 +1,84 @@
+struct Node {
+    string word;
+    int freq;
+    Node* prev;
+    Node* next;
+    Node(string k) : word(k), freq(1), prev(nullptr), next(nullptr) {}
+};
 class AllOne {
 public:
-    unordered_map<string, int> freq;
-    set<pair<int,string>> st;
+    Node *head, *tail;
+    unordered_map<string, Node*> um;
     AllOne() {
-
+        head = new Node("");
+        tail = new Node("");
+        head->next = tail;
+        tail->prev = head;
     }
-    void inc(string key) {
-        st.erase({freq[key],key});
-        freq[key]++;
-        st.insert({freq[key], key});
+    void moveToCorrectNextPosition(Node* node) {
+        Node* ptr = node->next;
+        while (ptr != tail && node->freq > ptr->freq) {
+            ptr = ptr->next;
+        }
+        if (ptr != node->next) {
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+            ptr->prev->next = node;
+            node->prev = ptr->prev;
+            node->next = ptr;
+            ptr->prev = node;
+        }
     }
-    void dec(string key) {
-        st.erase({freq[key],key});
-        freq[key]--;
-        if(freq[key] > 0){
-            st.insert({freq[key], key});
+    void moveToCorrectPrevPosition(Node* node) {
+        Node* ptr = node->prev;
+        while (ptr != head && node->freq < ptr->freq) {
+            ptr = ptr->prev;
+        }
+        if (ptr != node->prev) {
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+            ptr->next->prev = node;
+            node->next = ptr->next;
+            node->prev = ptr;
+            ptr->next = node;
+        }
+    }
+    void inc(string word) {
+        if (um.find(word) != um.end()) {
+            Node* node = um[word];
+            node->freq++;
+            moveToCorrectNextPosition(node);
+        } else {
+            Node* node = new Node(word);
+            node->next = head->next;
+            node->prev = head;
+            head->next->prev = node;
+            head->next = node;
+            um[word] = node;
+            moveToCorrectNextPosition(node);
+        }
+    }
+    void dec(string word) {
+        Node* node = um[word];
+        node->freq--;
+        moveToCorrectPrevPosition(node);
+        if (node->freq == 0) {
+            node->next->prev = node->prev;
+            node->prev->next = node->next;
+            um.erase(word);
+            delete node;
         }
     }
     string getMaxKey() {
-        if(st.size()==0){return "";}
-        string ans = prev(st.end())->second;
+        string ans = "";
+        if (tail->prev != head)
+            ans = tail->prev->word;
         return ans;
     }
     string getMinKey() {
-        if(st.size()==0){return "";}
-        string ans = st.begin()->second;
+        string ans = "";
+        if (head->next != tail)
+            ans = head->next->word;
         return ans;
     }
 };
