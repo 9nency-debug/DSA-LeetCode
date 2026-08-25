8b7f84e8e6dbb689f5c4ac79df94820b12810943
Nency <9.nency@gmail.com>
2026-08-25 15:38:59 +0530

    Time: 0 ms (100%), Space: 24.1 MB (67.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int max = *max_element(nums.begin(), nums.end());
-        for(int i = 1; i <= max + k; i++){
-            int mul = i * k;
-            if(find(nums.begin(), nums.end(), mul) != nums.end()){
-                continue;
-            } else {
-                return mul;
+        // int max = *max_element(nums.begin(), nums.end());
+        // for(int i = 1; i <= max + k; i++){
+        //     int mul = i * k;
+        //     if(find(nums.begin(), nums.end(), mul) != nums.end()){
+        //         continue;
+        //     } else {
+        //         return mul;
+        //     }
+        // }
+        // return k;
+
+        // >>> Second Approach:
+        unordered_map<int, bool> mp;
+        for (auto it : nums) {
+            if (it % k == 0) {
+                mp[it / k] = true;
             }
         }
-        return k;
+        int i = 1;
+        while (true) {
+            if (!mp[i]) {
+                return i * k;
+            }
+            i++;
+        }
     }
 };
