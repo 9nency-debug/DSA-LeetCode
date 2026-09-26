96ab38191c5a94b003fb518ee83ac416897332e4
Nency <9.nency@gmail.com>
2026-09-26 21:22:52 +0530

    Time: 119 ms (30.53%), Space: 144.8 MB (6.23%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+
+    string findValue(const string &charKey,
+                     unordered_map<string, string> &umap) {
+
+        auto it = umap.find(charKey);
+
+        if (it != umap.end()) {
+            return it->second;
+        }
+
+        return "?";
+    }
+
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+
+        string output = "";
+        int n = s.length();
+
+        bool foundKey = false;
+        string charKey = "";
+
+        unordered_map<string, string> umap;
+
+
+        for (auto it : knowledge) {
+            umap[it[0]] = it[1];
+        }
+
+
+        for (int i = 0; i < n; i++) {
+
+            if (s[i] == '(') {
+                foundKey = true;
+                continue;
+            }
+
+            if (foundKey) {
+
+                if (s[i] == ')') {
+
+                    output += findValue(charKey, umap);
+
+                    foundKey = false;
+                    charKey = "";
+
+                } else {
+
+                    charKey += s[i];
+                }
+
+            } else {
+
+                output += s[i];
+            }
+        }
+
+        return output;
+    }
+};
